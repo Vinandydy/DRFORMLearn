8b2f5139a15dea8b9d1fcbb025846c445dc8d824
Vinandydy <vinandy4718@gmail.com>
2025-09-16 16:23:44 +0300

    Update BooksApiTest

--- drf/main/tests/test_main.py
+++ drf/main/tests/test_main.py
@@ -6,8 +6,8 @@ from rest_framework.test import APITestCase
 
 class BooksApiTest(APITestCase):
     def test_get(self):
-        book_1 = Book.objects.create(title = 'test', author=1, pubilsher=1, price=1000.00, published_year=2004)
-        book_2 = Book.objects.create(title='test_2', author=2, pubilsher=2, price=2000.00, published_year=2004)
+        book_1 = Book.objects.create(title = 'test', author_id=1, publisher_id=1, price=1000.00, published_year=2004)
+        book_2 = Book.objects.create(title='test_2', author_id=2, publisher_id=2, price=2000.00, published_year=2004)
         url = reverse('book-list')
         response = self.client.get(url)
         serializer_data = BookSerialzer([book_1, book_2], many=True).data
