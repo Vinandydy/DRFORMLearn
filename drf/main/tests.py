c000c9000d112f8e1c28fe60cac7577e29b81eb2
Vinandydy <vinandy4718@gmail.com>
2025-09-16 15:42:36 +0300

    Few Unit Tests added

--- drf/main/tests.py
+++ drf/main/tests.py
@@ -11,3 +11,19 @@ class TestBook(TestCase):
     def test_equal(self):
         response = self.client.get('/book/equal/')
         self.assertEqual(response.status_code, 200)
+
+    def test_is_cheap(self):
+        response = self.client.get('/book/is_cheap_or_expensive/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_five_hundred(self):
+        response = self.client.get('/book/five_hundred/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_sales_income(self):
+        response = self.client.get('/book/sales_income/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_prefix(self):
+        response = self.client.get('/book/prefix/')
+        self.assertEqual(response.status_code, 200)
