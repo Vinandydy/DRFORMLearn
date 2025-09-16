7215db54a0fbae2fbfa3a8d961e557a5b2b76f11
Vinandydy <vinandy4718@gmail.com>
2025-09-16 13:25:38 +0300

    Few Unit Tests added

--- drf/main/tests.py
+++ drf/main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestBook(TestCase):
+
+    def test_book(self):
+        response = self.client.get('/book/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_equal(self):
+        response = self.client.get('/book/equal/')
+        self.assertEqual(response.status_code, 200)
